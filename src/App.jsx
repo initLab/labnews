1a325077237cdbc66f58569469e1aea210467ede
Vencislav Atanasov <user890104@users.noreply.github.com>
2025-10-24 22:56:05 +0300

    Fix punctuation

--- src/App.jsx
+++ src/App.jsx
@@ -76,10 +76,10 @@ function App() {
             padding: '10px',
             fontFamily: 'Arial, Verdana, Tahoma, Helvetica, sans-serif',
         }}>
-            <p>Ахой!</p>
+            <p>Здравейте!</p>
             <p>
                 <strong>Предстоящи събития в лаба:</strong><br />
-                {selectedEvents.length === 0 && '- още не е планирано нищо'}
+                {selectedEvents.length === 0 && 'Още не е планирано нищо.'}
                 {selectedEvents.map(event => <Fragment key={event.uid}>
                     - {format(event.start, 'd MMMM (EEEE) от HH:mm', {
                         locale: bg,
@@ -88,20 +88,20 @@ function App() {
             </p>
             <p>
                 <strong>Новини:</strong><br />
-                - заредили сме различни видове Club Mate и неща за хрупане
+                - Заредили сме различни видове Club Mate и неща за хрупане.
             </p>
             <p>
                 <strong>Обяви:</strong><br />
-                - няма нови, може да видите актуалните обяви тук: <a href="https://initlab.org/category/listings/">
+                - Няма нови. Може да видите актуалните обяви тук: <a href="https://initlab.org/category/listings/">
                 https://initlab.org/category/listings/</a>
             </p>
             <p>
                 <strong>Искаш да ни подкрепиш?</strong><br />
-                - <a href="https://initlab.org/%D0%B7%D0%B0-%D0%BD%D0%B0%D1%81/%D0%BF%D1%80%D0%B8%D1%81%D1%8A%D0%B5%D0%B4%D0%B8%D0%BD%D0%B8-%D1%81%D0%B5/">стани член</a><br />
-                - организирай събитие или курс - пиши ни на <a href="mailto:us@initlab.org">us@initlab.org</a> с идеята си!<br />
-                - <a href="https://initlab.org/%d0%b7%d0%b0-%d0%bd%d0%b0%d1%81/%d0%b4%d0%b0%d1%80%d0%b5%d0%bd%d0%b8%d1%8f/">подари ни нещо</a><br />
-                - разкажи на някого за нас<br />
-                - <a href="https://github.com/initlab/initlab/issues/">помогни ни с текущите проекти, задачи и проблеми на Лаба</a>
+                - <a href="https://initlab.org/%D0%B7%D0%B0-%D0%BD%D0%B0%D1%81/%D0%BF%D1%80%D0%B8%D1%81%D1%8A%D0%B5%D0%B4%D0%B8%D0%BD%D0%B8-%D1%81%D0%B5/">Стани член</a>;<br />
+                - Организирай събитие или курс - пиши ни на <a href="mailto:us@initlab.org">us@initlab.org</a> с идеята си;<br />
+                - <a href="https://initlab.org/%d0%b7%d0%b0-%d0%bd%d0%b0%d1%81/%d0%b4%d0%b0%d1%80%d0%b5%d0%bd%d0%b8%d1%8f/">Подари ни нещо</a>;<br />
+                - Разкажи на някого за нас;<br />
+                - <a href="https://github.com/initlab/initlab/issues/">Помогни ни с текущите проекти, задачи и проблеми на Лаба</a>.
             </p>
             <p>
                 <strong>Спонсори, дарители и Patreon подкрепчици от последния месец:</strong><br />
